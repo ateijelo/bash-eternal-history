bc530d5a792eb12dd38f26a3e034bfbe8fe6717c
Andy Teijelo Pérez <andy.teijelo@alayacare.com>
2018-01-31 14:33:09 -0500

    Initial commit

---
+++ bash-eternal-history.sh
@@ -0,0 +1,16 @@
+source ${BASH_SOURCE[0]%/*}/bash-preexec/bash-preexec.sh
+
+preexec() {
+    if [ -n "$MC_SID" ]; then return; fi # disable inside Midnight Commander
+    ENABLE_PRECMD="yes"
+    { printf "$$ $USER %(%s)T - " -1 ; history 1 ; } >> ~/.bash_eternal_history
+}
+
+precmd() {
+    if [ -n "$MC_SID" ]; then return; fi # disable inside Midnight Commander
+    if [ -n "$ENABLE_PRECMD" ]
+    then
+        { printf "$$ $USER - %(%s)T " -1 ; history 1 ; } >> ~/.bash_eternal_history
+        unset ENABLE_PRECMD
+    fi
+}
